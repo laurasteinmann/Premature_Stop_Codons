807efd0f4aeaa231f300502970e1846b6d6ce1f9
Laura Steinmann <zirkus.laura@web.de>
2022-02-28 13:37:06 +0100

    Remaining Length (Approach II)

--- scripts/Length_mRNA.py
+++ scripts/Length_mRNA.py
@@ -12,7 +12,8 @@ for row in ara11.index:
     short_name = name[:9]
     ara11.loc[row, 'Short_Name'] = short_name 
 
-results_table = pd.DataFrame(columns=['Chrom', 'Pos', 'Gene', 'Start_Protein', 'Stop_Protein', 'Length', 'Relative_Length'])
+results_table = pd.DataFrame(columns=['Chrom', 'Pos', 'Gene', 'Start_Protein', 'Stop_Protein', 
+                'Length', 'Relative_Length'])
 results_table['Chrom'] = stop_codons['Chrom']
 results_table['Pos'] = stop_codons['Pos']
 results_table['Gene'] = stop_codons['Gene']
@@ -20,10 +21,12 @@ results_table['Gene'] = stop_codons['Gene']
 for entry in results_table.index:
     gene = results_table.loc[entry, 'Gene']
     gene_attributes = ara11[ara11['Short_Name']==gene]
-    if gene_attributes.shape[0] >= 1:
-        gene_attributes = gene_attributes.iloc[0,]
-        results_table.loc[entry,'Start_Protein'] = gene_attributes['Start_Protein']
-        results_table.loc[entry, 'Stop_Protein'] = gene_attributes['Stop_Protein']
+    stop_position = results_table.loc[entry,'Pos']
+    possible_forms = gene_attributes[(gene_attributes['Start_Protein']<= stop_position) & (gene_attributes['Stop_Protein'] >= stop_position)]
+    if possible_forms.shape[0] >= 1:
+        gene_information  = possible_forms.iloc[0,]
+        results_table.loc[entry,'Start_Protein'] = gene_information['Start_Protein']
+        results_table.loc[entry,'Stop_Protein'] = gene_information['Stop_Protein']
     else:
         pass
 
@@ -31,15 +34,9 @@ results_table = results_table.dropna(subset=['Start_Protein', 'Stop_Protein'])
 
 for entry in results_table.index:
     stop_attributes = results_table.loc[entry,]
-    if stop_attributes.Start_Protein < stop_attributes.Stop_Protein:
-        length = stop_attributes.Pos - stop_attributes.Start_Protein
-        full_length = stop_attributes.Stop_Protein - stop_attributes.Start_Protein
-        relative_length = length/full_length * 100
-    else:
-        length = stop_attributes.Pos - stop_attributes.Stop_Protein
-        full_length = stop_attributes.Start_Protein - stop_attributes.Stop_Protein 
-        relative_length = length/full_length * 100
-
+    length = np.abs(stop_attributes.Pos - stop_attributes.Start_Protein)        
+    full_length = np.abs(stop_attributes.Stop_Protein - stop_attributes.Start_Protein)
+    relative_length = length/full_length * 100
     results_table.loc[entry, 'Length'] = length
     results_table.loc[entry, 'Relative_Length'] = relative_length 
 
