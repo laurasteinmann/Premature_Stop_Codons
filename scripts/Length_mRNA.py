5856b945cdba012cf278e0fa812762c32b74b4ed
Laura Steinmann <zirkus.laura@web.de>
2022-03-07 14:48:56 +0100

    Figure Relative Length Approach

--- scripts/Length_mRNA.py
+++ scripts/Length_mRNA.py
@@ -12,8 +12,8 @@ for row in ara11.index:
     short_name = name[:9]
     ara11.loc[row, 'Short_Name'] = short_name 
 
-results_table = pd.DataFrame(columns=['Chrom', 'Pos', 'Gene', 'Start_Protein', 'Stop_Protein', 
-                'Length', 'Relative_Length'])
+results_table = pd.DataFrame(columns=['Chrom', 'Pos', 'Gene', 'Start_Protein',           
+'Stop_Protein', 'Length', 'Relative_Length'])
 results_table['Chrom'] = stop_codons['Chrom']
 results_table['Pos'] = stop_codons['Pos']
 results_table['Gene'] = stop_codons['Gene']
@@ -41,3 +41,11 @@ for entry in results_table.index:
     results_table.loc[entry, 'Relative_Length'] = relative_length 
 
 results_table.to_csv('../data/processed/Relative_Lengths/Stop_Codons_Full_List_Lengths.csv')
+
+results_table_threshold = results_table[results_table['Relative_Length']>=30]
+results_table_threshold.to_csv('../data/processed/Relative_Lengths/Stop_Codons_30percent_Threshold.csv')
+results_table_sorted = results_table.sort_values(by='Relative_Length')
+results_table_equal = results_table_sorted.iloc[0:7124,]
+threshold_2 = results_table_equal.iloc[-1,]['Relative_Length']
+print("Second_Threshold: ", threshold_2)
+results_table_equal.to_csv('../data/processed/Relative_Lengths/Stop_Codons_Equal_Number_Approach1.csv')
