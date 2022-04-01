b6462a5fdba4320cc34db5f4261b9241c872602b
Laura Steinmann <zirkus.laura@web.de>
2022-04-01 17:12:40 +0200

    Control Synonymous Mutations

--- scripts/Coocurrence_Analysis.py
+++ scripts/Coocurrence_Analysis.py
@@ -14,3 +14,15 @@ coocurrence_significant = coocurrence_significant.replace('diff_Chr', 1000000)
 coocurrence_significant["Distance"] = pd.to_numeric(coocurrence_significant["Distance"])
 coocurrence_significant = coocurrence_significant[(coocurrence_significant['Distance'] > 10000)]
 coocurrence_significant.to_csv('../data/processed/Coocurrence/Coocurrence_Table_Bonferroni_Selected_Interactions.csv')
+
+coocurrence_table_less = pd.read_csv('../data/processed/Coocurrence/Coocurrence_Table_Significant_Full.csv', index_col=0)
+pvaluethreshold2 = 0.05
+coocurrence_significant_less = coocurrence_table_less[coocurrence_table_less['pvalue_upper']<= pvaluethreshold2]
+coocurrence_significant_less = coocurrence_significant_less[coocurrence_significant_less['Gene1']!=coocurrence_significant_less['Gene2']]
+coocurrence_significant_less = coocurrence_significant_less.replace('diff_Chr', 1000000)
+coocurrence_significant_less["Distance"] = pd.to_numeric(coocurrence_significant_less["Distance"])
+
+coocurrence_significant_under = coocurrence_table_less[coocurrence_table_less['pvalue_lower']<= pvaluethreshold2]
+coocurrence_significant_under = coocurrence_significant_under[coocurrence_significant_under['Gene1']!=coocurrence_significant_under['Gene2']]
+coocurrence_significant_under = coocurrence_significant_under.replace('diff_Chr', 1000000)
+coocurrence_significant_under["Distance"] = pd.to_numeric(coocurrence_significant_under["Distance"])
--- scripts/Preprocessing_SNPs.py
+++ scripts/Preprocessing_SNPs.py
@@ -1,18 +1,65 @@
 # Control with Synonmymous and Nonsynonymous SNP mutations
 # Library
 import pandas as pd
+import numpy as np 
 # Function
 def generate_summary_table():
-    table = pd.DataFrame(columns=['Chrom', 'Pos', 'ID'])
+    table = pd.DataFrame(columns=['Chrom', 'Pos', 'ID', 'Type', 'ALT_Frequency'])
     return table
 
-def fill_snps(table, data):
-    for snp in data.index:
-        table.loc[snp,'Chrom'] = data.loc[snp,'CHROM']
-        table.loc[snp, 'Pos'] = data.loc[snp, 'POS']
-        table.loc[snp,'ID'] = str(table.loc[snp,'Chrom']) + '-' + str(table.loc[snp, 'Pos'])
+def fill_out_summary_table(table, data):
+    table['Chrom'] = data['Chromosome']
+    table['Pos'] = data['Position']
+    table['ID'] = data['SNP']
+    table['Type'] = data['SYN_vs_NON_SYN']
     return table
+
+def filter_accessions(gt_section, expression_data):
+    gt_section_first_filter = gt_section.iloc[:, 1:]
+    expression_accessions = expression_data.index.values
+    gt_accessions = gt_section_first_filter.columns.values
+    expression_accessions = expression_accessions.astype(int)
+    gt_accessions = gt_accessions.astype(int)
+    gt_section_first_filter.columns = gt_accessions
+    in_both = gt_accessions[np.isin(gt_accessions,expression_accessions)]
+    gt_section_first_filter = gt_section_first_filter[in_both]
+    return gt_section_first_filter
+
+def make_ids(chrom, position):
+    ids = np.empty(shape=0)
+    for i in np.arange(0, len(chrom)):
+        id = chrom[i] + '- ' + position[i]
+        ids = np.append(ids, id)
+    return ids
 # Analysis
-fixed_section = pd.read_csv('data/preprocessed/Fixed_Section_1001_Genomes.csv', index_col=0)
+snps_attributes = pd.read_csv('../data/preprocessed/snps_2029.csv', index_col=0)
+synonymous = snps_attributes[snps_attributes['SYN_vs_NON_SYN']=='SYN']
 summary_table = generate_summary_table()
-summary_table = fill_snps(summary_table, fixed_section)
+summary_table = fill_out_summary_table(summary_table, synonymous)
+print('Summary table done:', summary_table.shape)
+summary_table.to_csv('../data/processed/Synonymous_Table.csv')
+
+gt_section = pd.read_csv('../data/preprocessed/GT_Section_1001_Genomes.csv', index_col=0)
+print('GT_Section geladen:', gt_section.iloc[0:6,0:6])
+expression_data = pd.read_csv('../data/preprocessed/727_Expressiondata.csv', index_col=0)
+gt_section_665 = filter_accessions(gt_section, expression_data)
+gt_section_665.to_csv('../data/preprocessed/665_GT_Section.csv')
+
+fixed_section = pd.read_csv('../data/preprocessed/Fixed_Section_1001_Genomes.csv', index_col=0)
+subset = fixed_section.iloc[400000:500000,:]
+chrom = subset.CHROM.values
+chrom = chrom.astype(str)
+position = subset.POS.values
+position = position.astype(str)
+ids_array = make_ids(chrom, position)
+
+fixed_section.iloc[400000:500000,2] = ids_array
+fixed_section.to_csv('../data/preprocessed/Fixed_Section_1001_Genomes.csv')
+
+#ids_synonymous = summary_table.ID.values
+#ids_synonymous = ids_synonymous.astype(str)
+
+#in_both = ids_synonymous[np.isin(ids_synonymous, id_array)]
+#genomics2 = genomics.loc[in_both]
+#genomics2.shape
+#genomics2.to_csv('../data/preprocessed/Second_Filter_Step.csv')
